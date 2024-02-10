8fc0078cc6beb360174ff165926b7a62eab1f335
tkzc00 <2238983030@qq.com>
2024-02-10 13:43:05 +0800

    add: 前端整合ant-design-vue组件库

--- kong-search-frontend/src/main.ts
+++ kong-search-frontend/src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/reset.css";
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+app.use(Antd);
+app.use(router).mount("#app");
